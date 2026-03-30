326ef4d1d5ca09fc2556d0df3794c4550889f513
Mubarek Jemal <mubarekj71@gmail.com>
2026-03-30 11:54:00 +0300

    initial project setup with proper structure

---
+++ bin/student_manager_cli.dart
@@ -0,0 +1,6 @@
+
+
+import 'package:student_manager_cli/student_manager_cli.dart';
+void main() {
+  startApp();
+}
---
+++ lib/student_manager_cli.dart
@@ -0,0 +1,14 @@
+
+
+class  Student {
+  String? name;
+  int? age;
+
+  Student(this.name, this.age);
+}
+
+List<Student> students = [];
+
+void startApp(){
+  print("App started...");
+}
