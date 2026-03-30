b8aca3c30b0c1a6a0036da415e11d8eec00d017c
Mubarek Jemal <mubarekj71@gmail.com>
2026-03-30 20:53:11 +0300

    added view student feature

--- lib/student_manager_cli.dart
+++ lib/student_manager_cli.dart
@@ -20,3 +20,29 @@ void addStudent(String name, int age) {
   print("Student is added!");
 }
 
+// View Students function
+void viewStudents() {
+  if (students.isEmpty) {
+    print("No students found.");
+    return;
+  }
+  // for(int i = 0; i<students.length; i++){
+  //   print("Name: ${students[i].name}, Age: ${students[i].age}");
+  // }
+  for (Student student in students) {
+    print("Name: ${student.name}, Age: ${student.age}");
+  }
+}
+
+//for instance if we want to filter adult students over 20. Here is how you would print only the students who are older than 20
+void viewAdultStudents() {
+  // 1. Create a filtered version of the list
+  var adults = students.where((s) => s.age! > 20);
+
+  // 2. Loop through ONLY those students
+  for (var student in adults) {
+    print("Adult student Found - Name: ${student.name}, Age: ${student.age}");
+  }
+}
+
+
