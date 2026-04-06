502258f9060028305f4582b056fb8379fb7aa60d
Mubarek Jemal <mubarekj71@gmail.com>
2026-04-06 15:44:41 +0300

    Final cli

--- bin/student_manager_cli.dart
+++ bin/student_manager_cli.dart
@@ -3,13 +3,4 @@ import 'package:student_manager_cli/student_manager_cli.dart';
 
 void main() async{
   await startApp();
-  // addStudent("Mubarek", 23);
-  // viewStudents();
-  // addStudent("Tola", 30);
-  // viewStudents(4);
-  // deleteStudent("Tola");
-  // viewStudents();
-  // deleteStudent("Abichaw");
-  // viewAdultStudents();
-  
 }
